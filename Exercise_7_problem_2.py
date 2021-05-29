f3b90e0afe6bc8268393daa505f600a8363c40ba
masashi719 <>
2021-05-29 09:06:30 +0000

    I finished the all.

--- Exercise_7_problem_2.py
+++ Exercise_7_problem_2.py
@@ -17,7 +17,12 @@
 # - Parse dates from the column `'DATE'` and set the dates as index in the dataframe 
 
 # YOUR CODE HERE 1 to read the data into data and parse dates
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+fp="data/helsinki-vantaa.csv"
+# read the file. and analysis　'DATE' index change 'DATE'.
+data=pd.read_csv(fp,parse_dates=['DATE'],index_col='DATE')
 # This test print should print first five rows
 print(data.head())
 
@@ -31,7 +36,10 @@ print(len(data))
 # - Store the selection in a new variable `selection`
 
 # YOUR CODE HERE 2
-
+# create new colum because index is not int  
+data["date"]=pd.to_datetime(data.index)
+# select the data from 1988 to 2018
+selection=data.loc[(data["date"].dt.year>=1988)&(data["date"].dt.year<=2018)]
 # Check that the data was read in correctly:
 selection.head()
 
@@ -51,13 +59,41 @@ print("Number of rows:", len(selection))
 # 
 
 # YOUR CODE HERE 3
+# create the table data
+monthly_data=pd.DataFrame()
+# Change datatype and use slice method to get the value that i want to get
+selection['TIME_STR']=selection['date'].astype(str)
+# Cut out each value
+selection['YEAR']=selection['TIME_STR'].str.slice(start=0,stop=4)
+# Cut out each value
+selection['MONTH']=selection['TIME_STR'].str.slice(start=4,stop=6)
+# Put together the data
+grouped=selection.groupby(['YEAR','MONTH'])
+# Put a value
+mean_col=['TEMP_C']
+# calculate the average for all group's TEMP_C
+for key,group in grouped:
+  # get the mean of TEMP_C
+  mean_values=group[mean_col].mean()
+  # Add mean_value to monthly_data
+  monthly_data=monthly_data.append(mean_values,ignore_index=True)
+selection_data=pd.DataFrame({'x':selection["date"],'y':selection["TEMP_C"]})
 
+# setting of figure
+selection_data.plot.line(x='x',y='y'
+,style=['k.-'])
+
+# change the tittle, xlabel and ylabel
+plt.title("Helsinki-Vantaa Airport")
+plt.xlabel("Time")
+plt.ylabel("Temperature(Celsius)")
 # Set output file name
-outputfp = ""
+outputfp = "temp_line_plot.png"
 
 # Save plot as image
 # YOUR CODE HERE 4
-
+# save the data 
+plt.savefig("temp_line_plot.png")
 import os
 
 #Check that output file exists (also open the file and check that the plot looks ok!)
